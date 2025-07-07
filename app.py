b35f1ab515162a6bd03081b5c7584bbc3327a28a
Fahad Bin Islam <fahadbinislam16@gmail.com>
2025-07-07 11:47:05 -0400

    Initial commit for Captor Signature App

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import os
+import base64
+
+app = Flask(__name__, static_folder='static')
+
+UPLOAD_FOLDER = 'signatures'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def homepage():
+    return app.send_static_file('index.html')
+
+@app.route('/save-signature', methods=['POST'])
+def save_signature():
+    data = request.json
+    name = data['name'].strip().replace(" ", "_")
+    image_data = data['image'].split(',')[1]  # remove base64 header
+    image_bytes = base64.b64decode(image_data)
+
+    filename = f"{name}.png"
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+    with open(filepath, 'wb') as f:
+        f.write(image_bytes)
+
+    return jsonify({"message": f"Signature saved as {filename}!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
